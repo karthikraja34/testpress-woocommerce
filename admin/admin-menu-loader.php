360d66a5104baa48054c95979496fc907d8c58c0
thamizhsoftsuave <thamizh@softsuave.com>
2021-01-24 23:28:16 +0530

    Get all product list

--- admin/admin-menu-loader.php
+++ admin/admin-menu-loader.php
@@ -8,11 +8,28 @@
 
 
    function admin_menu_main() {
-         product_table();
+
+         get_products();
 
    }
 
-      function product_table() {
+   function get_products() {
+    $query = array(
+                  'post_type'      => 'product',
+                  'posts_per_page' => -1
+              );
+              $productslist = new WP_Query( $query );
+              wp_reset_query();
+
+            if ( empty( $productslist ) ) {
+              return;
+           } else {
+                 product_table($productslist);
+           }
+
+   }
+
+      function product_table($productslist) {
 
        ?>  <div class="wrap">
                  <h2><?php esc_attr_e( 'Products', 'WpAdminStyle' ); ?></h2>
@@ -21,13 +38,23 @@
                  	<tr>
                  		<th class="row-title"><?php esc_attr_e( 'Icon' ); ?></th>
                  		<th><?php esc_attr_e( 'Name' ); ?></th>
+                 		<th><?php esc_attr_e( 'SKU' ); ?></th>
+                 		<th><?php esc_attr_e( 'Price' ); ?></th>
+                 		<th><?php esc_attr_e( 'Stock' ); ?></th>
                  	</tr>
                  	</thead>
                  	<tbody>
+                 	<?
+                 	while ( $productslist->have_posts() ) : $productslist->the_post();
+                                     global $product; ?>
                  	<tr>
-                 		<td class="row-title"><label for="tablecell"><?php echo'<img class="wp-post-image" src="https://homepages.cae.wisc.edu/~ece533/images/airplane.png"></img>'; ?></label></td>
-                     <td class="row-title"><label for="tablecell"><?php esc_attr_e('Shirt'); ?></label></td>
+                 	<td class="row-title"><label for="tablecell"><span class='wp-post-image'><?php echo woocommerce_get_product_thumbnail(); ?></span></label></td>
+                     <td class="row-title"><label for="tablecell"><?php echo get_the_title(); ?></label></td>
+                     <td class="row-title"><label for="tablecell"><?php echo  $product->get_sku(); ?></label></td>
+                     <td class="row-title"><label for="tablecell"><?php echo  $product->get_regular_price(); ?></label></td>
+                     <td class="row-title"><label for="tablecell"><?php echo  $product->get_stock_status(); ?></label></td>
                  	</tr>
+                 	 <? endwhile; ?>
                  	</tbody>
                  </table>
                  </div>
