e662ee0b95356ff5edcf4499bb11edb323fba725
thamizhsoftsuave <thamizh@softsuave.com>
2021-01-31 15:35:13 +0530

    Refactore the order_details to order

--- admin/order_complete_hook.php
+++ admin/order_complete_hook.php
@@ -6,20 +6,20 @@ add_action( 'woocommerce_thankyou', 'after_order_complete', 10, 1 );
 function after_order_complete( $order_id ) {
 	$endpoint = 'https://hookb.in/K3GDQVEojZu0zzW3V9Bo';
 
-	$order_details = get_order_details( $order_id );
+	$order = get_order( $order_id );
 
-	if ( $order_details ) {
+	if ( $order ) {
 		$requestItem                 = new stdClass();
-		$requestItem->order_id       = $order_details->get_id();
-		$requestItem->order_total    = $order_details->get_total();
-		$requestItem->order_currency = $order_details->get_currency();
+		$requestItem->order_id       = $order->get_id();
+		$requestItem->order_total    = $order->get_total();
+		$requestItem->order_currency = $order->get_currency();
 
 		// call remote endpoint to update
 		post_endpoint_request( $requestItem, $endpoint );
 	}
 }
 
-function get_order_details( $order_id ) {
+function get_order( $order_id ) {
 	// get order detail
 	$order = wc_get_order( $order_id );
 
